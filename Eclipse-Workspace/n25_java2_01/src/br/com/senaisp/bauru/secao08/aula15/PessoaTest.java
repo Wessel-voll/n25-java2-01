a75c94f770cdf822641e789d429dde677a34bb96
Eduardo Wessel <wesseleduardo@gmail.com>
2025-08-21 21:46:07 -0300

    PessoaFisicaJuridica

--- Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao08/aula15/PessoaTest.java
+++ Eclipse-Workspace/n25_java2_01/src/br/com/senaisp/bauru/secao08/aula15/PessoaTest.java
@@ -3,17 +3,25 @@ package br.com.senaisp.bauru.secao08.aula15;
 public class PessoaTest {
 
 	public static void main(String[] args) {
-		Pessoa pe01 = new Pessoa();
-		Pessoa pe02 = new Pessoa();
+		Pessoa pe01 = new PessoaFisica();
+		Pessoa pe02 = new PessoaJuridica();
 		pe01.setNome("Roberval da Silva");
 		pe01.setEndereco("Rua das Ruas, 1234");
-		pe01.setDocumento("123.456.789-09");
+		try {
+			pe01.setDocumento("123.456.789-09");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		pe01.setDataNascimento("20/05/1999");
 		
-		pe02.setNome("Roberval da Silva");
-		pe02.setEndereco("Rua das Ruas, 1234");
-		pe02.setDocumento("123.456.789-09");
-		pe02.setDataNascimento("20/05/1999");
+		pe02.setNome("Roberval da Silva dnv");
+		pe02.setEndereco("Rua das Ruas, 4321");
+		try {
+			pe02.setDocumento("12345678908");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		pe02.setDataNascimento("20/05/9991");
 		
 		System.out.println(pe01);
 		System.out.println(pe02);
